e8256cae6f5f2162af070ee347eaf2e65cd2f7df
deyizhang <deyizhang2003@gmail.com>
2020-04-27 22:02:12 -0400

    Commit

--- xylophone_app/ios/Flutter/flutter_export_environment.sh
+++ xylophone_app/ios/Flutter/flutter_export_environment.sh
@@ -5,6 +5,7 @@ export "FLUTTER_APPLICATION_PATH=C:\Users\deyiz\AndroidStudioProjects\xylophone_
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build\ios"
+export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
 export "FLUTTER_FRAMEWORK_DIR=C:\flutter_windows_v1.9.1+hotfix.6-stable\bin\cache\artifacts\engine\ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
--- xylophone_app/lib/main.dart
+++ xylophone_app/lib/main.dart
@@ -1,14 +1,69 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audio_cache.dart';
 
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  void sound(int soundnumber){
+      final player = AudioCache();
+      player.play("note$soundnumber.wav");
+  }
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Center(
+            child: Container(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  FlatButton(
+                    color: Colors.red,
+                    onPressed: () {
+                      sound(1);
+                    },
+                  ),
+                  FlatButton(
+                    color: Colors.orange,
+                    onPressed: () {
+                      sound(2);
+                    },
+                  ),
+                  FlatButton(
+                    color: Colors.yellow,
+                    onPressed: () {
+                      sound(3);
+                    },
+                  ),
+                  FlatButton(
+                    color: Colors.green,
+                    onPressed: () {
+                      sound(4);
+                    },
+                  ),
+                  FlatButton(
+                    color: Colors.teal,
+                    onPressed: () {
+                      sound(5);
+                    },
+                  ),
+                  FlatButton(
+                    color: Colors.blue,
+                    onPressed: () {
+                      sound(6);
+                    },
+                  ),
+                  FlatButton(
+                    color: Colors.purple,
+                    onPressed: () {
+                      sound(7);
+                    },
+                  ),
+                ],
+              ),
+            ),
+          ),
         ),
       ),
     );
